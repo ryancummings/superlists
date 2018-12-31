ca283ec468437edf862cd2a71eee4257921ba0a2
Ryan Cummings <rcummings.04@gmail.com>
2018-12-31 12:40:29 -0500

    Fully functional duplicate testing with improved forms and tests

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,10 +5,16 @@ from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR,
+    EMPTY_ITEM_ERROR,
+    ExistingListItemForm,
+    ItemForm
+)
 
 from unittest import skip
 
+    
 
 
 class HomePageTest(TestCase):
@@ -23,6 +29,13 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
 
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            f'/lists/{list_.id}/',
+            data={'text': ''}
+        )
+    
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
@@ -51,13 +64,6 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
-    def post_invalid_input(self):
-        list_ = List.objects.create()
-        return self.client.post(
-            f'/lists/{list_.id}/',
-            data={'text': ''}
-        )
-
     def test_for_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
         self.assertEqual(Item.objects.count(), 0)
@@ -78,10 +84,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -90,13 +95,11 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
             )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
-class NewListTest(TestCase):
-    
     def test_can_save_a_POST_request(self):
         response = self.client.post('/lists/new', data={'text': 'A new list item'})
 
@@ -119,8 +122,8 @@ class NewListTest(TestCase):
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
     def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         
     def test_inalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})

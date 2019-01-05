df5d117b2ada7bf66fb9b7270f4b2887510cc509
Ryan Cummings <rcummings.04@gmail.com>
2019-01-04 22:57:03 -0500

    further debugging

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -16,7 +16,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             print(staging_server)
-            self.live_server_url = 'http://' + staging_server
+            self.live_server_url = 'http://' + self.staging_server
 
     def tearDown(self):
         self.browser.quit()
